a7b6d124819964576788107248124ad8a1b146b7
Erich Kopp <erichkopp@gmail.com>
2018-09-22 00:02:56 -0400

    Improve responsive grid layout, add User avatar support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
 	
 protected
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :bio, :location])
-		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :bio, :location])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :bio, :location, :avatar])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :bio, :location, :avatar])
 
 	end
 end
