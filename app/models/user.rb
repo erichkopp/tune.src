5e490b69a2e6c4f36bfb7cb4533da0416e30cf21
Erich Kopp <erichkopp@gmail.com>
2018-10-09 11:45:42 -0400

    Put sidenav in a partial. DRY

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tunes
+  has_many :tunes, dependent: :destroy
   has_one_attached :avatar
 end
